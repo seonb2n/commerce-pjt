51242ce522db12012d9927ad3d42c0be29c60a18
seonb2n <626ksb@gmail.com>
2024-04-07 11:21:03 +0900

    📝 docs: 프로젝트 생성 및 요구 기능 작성

---
+++ src/main/java/com/example/commercepjt/CommercePjtApplication.java
@@ -0,0 +1,13 @@
+package com.example.commercepjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercePjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommercePjtApplication.class, args);
+    }
+
+}
