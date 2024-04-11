5717a1385db57489b608fdba080ee256e4d499e1
seonb2n <626ksb@gmail.com>
2024-04-11 22:54:45 +0900

    📝 docs: 구현할 기능인 facade service 명세 정의

---
+++ src/main/java/com/example/commercepjt/service/BuyerFacadeService.java
@@ -0,0 +1,44 @@
+package com.example.commercepjt.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BuyerFacadeService {
+
+    /**
+     * 추천 상품 목록을 페이지로 조회할 수 있다.
+     */
+    void getRecommandedProductList() {}
+
+    /**
+     * 상품을 카테고리로 조회할 수 있다.
+     * @param categoryId
+     */
+    void getProductListByCategory(long categoryId) {}
+
+    /**
+     * 상품을 키워드로 검색할 수 있다.
+     * @param keyword
+     */
+    void getProductListByKeywork(String keyword) {}
+
+    /**
+     * 상품 구매 후기를 작성할 수 있다.
+     * @param productId
+     * @param orderId
+     * @param content
+     */
+    void createProductReview(long productId, long orderId, String content) {}
+
+    /**
+     * 상품 배송 현황을 확인할 수 있다.
+     * @param orderId
+     */
+    void checkProductOrderStatus(long orderId) {}
+
+    /**
+     * 배송 시작 전 단계의 상품을 환불할 수 있다.
+     * @param orderId
+     */
+    void refundProductBeforeDelivery(long orderId) {}
+}
